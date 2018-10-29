e354e580238de9d13322f943635cb703214daaf6
mathieumllt <mathieu.maillet@ymail.com>
2018-10-29 19:49:25 +0100

    links fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   
   get '/', to: 'static#index'
-  get 'static/team', to: 'static#team'
-  get 'static/contact', to: 'static#contact'
+  get '/team', to: 'static#team'
+  get '/contact', to: 'static#contact'
 
-  get 'welcome/:first_name', to: 'welcome#first_name'
+  get '/welcome/:first_name', to: 'welcome#first_name'
 
   get '/gossip/:id', to: 'gossip#id', as: 'gossip'
 
